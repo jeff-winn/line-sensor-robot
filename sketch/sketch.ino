758c8b93d04b4fd2125df132715e2f1fda1806f2
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2024-09-11 21:59:26 -0400

    Adding debugger settings.

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,9 +1,14 @@
+#include <Adafruit_FRAM_I2C.h>
+
 void setup()
 {
 	// Setup what it needs to use here.
+
+    auto i = 0;
+    auto x = 1;
 }
 
 void loop()
 {
-    // Do what it's supposed to do here.
+    // Do what it's supposed to do here.    
 }
