ffeac0ecfcdd43b65672024d82c267b558a44a93
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2024-09-12 21:18:22 -0400

    Updates.

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,14 +1,11 @@
-#include <Adafruit_FRAM_I2C.h>
+int i;
 
 void setup()
 {
-	// Setup what it needs to use here.
-
-    auto i = 0;
-    auto x = 1;
+    i = 0;
 }
 
 void loop()
 {
-    // Do what it's supposed to do here.    
+    i++;
 }
